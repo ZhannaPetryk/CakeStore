b6a733a16d3dcd0a371b00d93a94704c43a5fd98
ZhannaPetryk <boyko.joanna@gmail.com>
2021-07-23 17:33:15 +0300

    Remove unused references

--- StoreTestWPF.DAL/Models/Cake.cs
+++ StoreTestWPF.DAL/Models/Cake.cs
@@ -1,10 +1,7 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 
 namespace StoreTestWPF.DAL.Models
 {
